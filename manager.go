8468c9af1fe07c710270edd5f691c228bb20e9b5
Steve McCarthy <steve@redlua.com>
2016-02-12 18:23:22 -0500

    Add total hit counter and atomic two minute hit counter

--- manager.go
+++ manager.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
+	"sync/atomic"
 	"time"
 )
 
 func DataManager(in chan ProcessedLine) {
 	// setup
 	reqInfo := map[string]int{}
+	var (
+		totalHits     uint64 = 0
+		twoMinuteHits uint64 = 0
+	)
 
 	// recur
 	timer := make(chan struct{}, 1)
@@ -17,7 +22,12 @@ func DataManager(in chan ProcessedLine) {
 		select {
 		case <-timer:
 			fmt.Println(reqInfo)
+			fmt.Println("total hits: ", totalHits)
+			fmt.Println("two minute: ", atomic.LoadUint64(&twoMinuteHits))
 		case l := <-in:
+			totalHits += 1
+			atomic.AddUint64(&twoMinuteHits, 1)
+			go SubtractAfterTwoMinutes(&twoMinuteHits)
 			reqInfo[l.Section] += 1
 		}
 	}
@@ -28,4 +38,9 @@ func TenSecondTimer(timer chan<- struct{}) {
 		timer <- struct{}{}
 		time.Sleep(10 * time.Second)
 	}
+}
+
+func SubtractAfterTwoMinutes(v *uint64) {
+	time.Sleep(2 * time.Minute)
+	atomic.AddUint64(v, ^uint64(0)) // subtracts 1
 }
