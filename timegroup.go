56416ba3ecdee03476dee873b8e505fcf643b693
Steve McCarthy <steve@redlua.com>
2016-02-18 22:15:01 -0500

    Split TimeGroup to separate file

---
+++ timegroup.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"time"
+)
+
+// A TimeGroup keeps track of the number of hits that have occured within a
+// particular time range. In this case, Config.GroupingResolution. This was
+// added to greatly reduce memory requirements during large traffic spikes.
+type TimeGroup struct {
+	Time  time.Time
+	Count uint64
+}
+
+func GroupByResolution(ts []TimeGroup, t time.Time,
+	resolution time.Duration) []TimeGroup {
+	l := len(ts)
+	if l > 0 && t.Sub(ts[l-1].Time) <= resolution {
+		ts[l-1].Count += 1
+		return ts
+	} else {
+		return append(ts, TimeGroup{Time: t, Count: 1})
+	}
+}
+
+func RemoveOldTimeGroups(ts []TimeGroup, interval time.Duration) []TimeGroup {
+	n := time.Now().Add(-interval)
+	i := 0
+	for ; i < len(ts); i++ {
+		if ts[i].Time.After(n) {
+			break
+		}
+	}
+	return ts[i:]
+}
+
+func SumTimeGroup(ts []TimeGroup) (sum uint64) {
+	for _, t := range ts {
+		sum += t.Count
+	}
+	return
+}
